f3f1fdff24034eb14def75fdff3b2706ebdcc463
SPHERE-HQ <rasyaxdentaxcode@gmail.com>
2026-04-25 09:14:28 +0700

    feat: topup redirect to wallet coming soon

--- mobile/lib/modules/wallet/presentation/screens/topup_screen.dart
+++ mobile/lib/modules/wallet/presentation/screens/topup_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'wallet_screen.dart';
-
-class TopupScreen extends StatelessWidget {
-  const TopupScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const WalletScreen();
-}
+  import 'wallet_screen.dart';
+  class TopupScreen extends StatelessWidget {
+    const TopupScreen({super.key});
+    @override
+    Widget build(BuildContext context) => const WalletScreen();
+  }
+  
