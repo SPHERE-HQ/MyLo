0932d1998450e280c553af4feff6ae02713b6d7c
SPHERE-HQ <rasyaxdentaxcode@gmail.com>
2026-04-26 19:28:13 +0700

    fix: hapus unused import go_router di browser_screen

--- mobile/lib/modules/browser/presentation/screens/browser_screen.dart
+++ mobile/lib/modules/browser/presentation/screens/browser_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../../../../app/theme.dart';
 import '../../../../core/api/api_client.dart';
